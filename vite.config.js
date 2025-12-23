873c4491cb8971d48fa60c1da1df94fc81e400bd
viet2005-68 <23020170@vnu.edu.vn>
2025-12-23 22:33:29 +0700

    haizz

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://github.com/viet2005-68/TRAC_NGHIEM_KTCT_MMT.git',
   optimizeDeps: {
     include: ['mammoth', 'pdfjs-dist'],
     exclude: ['pdfjs-dist/build/pdf.worker.min.mjs']
